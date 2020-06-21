ddbbf39729800c0f75134b42d438522f00cb1b07
gmwu185 <gmwu185@gmail.com>
2020-06-21 23:14:54 +0800

    完成 carousel 元件

--- source/assets/js/custom.js
+++ source/assets/js/custom.js
@@ -1,5 +1,10 @@
 console.log('js all');
 
 $(function () {
-  console.log('jquery run');  
+  console.log('jquery run');
+
+  /** carousel/#methods
+    ** https://getbootstrap.com/docs/4.5/components/carousel/#methods
+  */
+  $('#carouselControls').carousel('pause');
 });
